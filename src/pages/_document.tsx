3ed3e5fa11862726594da11f80bca41f996c62a3
ndzhwr <ndizihiweregis06@gmail.com>
2023-01-11 10:36:42 +0200

    fixed fixables

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from "react";
 // eslint-disable-next-line @next/next/no-head-import-in-document
-import Head from "next/head";
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-    return (
 
-        <Head>
-            <title>Meta Tag Example</title>
-            <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
-            <meta name="google" content="notranslate" key="notranslate" />
-        </Head>
-    )
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
+                    <meta name="google" content="notranslate" key="notranslate" />
+                </Head >
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+
+        )
+    }
 }
